588060f0c784406d3705c39f058abf598f857648
your.name <leviwc3@hotmail.com>
2020-02-29 11:33:49 -0300

    Updates

---
+++ precache-manifest.978c975514597686a5ec018446d3557f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "884bbdfe591320595223365414fdedc7",
+    "url": "/CursoEngProd/index.html"
+  },
+  {
+    "revision": "02850e2ae72e724afdca",
+    "url": "/CursoEngProd/static/css/main.6e722a10.chunk.css"
+  },
+  {
+    "revision": "161f2c34e09e154dbe4c",
+    "url": "/CursoEngProd/static/js/2.f9071a03.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/CursoEngProd/static/js/2.f9071a03.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "02850e2ae72e724afdca",
+    "url": "/CursoEngProd/static/js/main.cb1f56d3.chunk.js"
+  },
+  {
+    "revision": "e823efb28ba4221acacd",
+    "url": "/CursoEngProd/static/js/runtime-main.3924100e.js"
+  }
+]);
